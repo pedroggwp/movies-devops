605349dede6f6c48dfafd2639d15b2b832c9a3ef
pedroggwp <phsnnascimento@outlook.com>
2024-08-09 13:02:42 -0300

    projeto flask cloud

--- webapp/server.py
+++ webapp/server.py
@@ -5,12 +5,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 def get_db_connection():
-    conn = psycopg2.connect(host='db',
-                            database='movies',
-                            user='postgres',
-                            password='Postgres2023')
+    conn = psycopg2.connect(host=os.getenv("DB_HOST"),
+                            database=os.getenv("DB_DATABASE"),
+                            user=os.getenv("DB_USERNAME"),
+                            password=os.getenv("DB_PASSWORD"))
     return conn
-
 
 @app.route('/')
 def index():
